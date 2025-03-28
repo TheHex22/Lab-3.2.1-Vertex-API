517c896f25a7feb85b0391bdb2f79abd73020e3b
TheHex22 <hmoreno7@techexchange.in>
2025-03-28 15:31:29 -0600

    Create vertex_app.py

---
+++ vertex_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+
+st.title("Find your neighboring states")
+
+users_state = st.text_input("Enter your state")
+
+
+# Section A: Add in your Vertex AI API call below
+
+
+# End of Section A
+
+
+st.write("The neighboring states are:")
+
+
+# Section B:  Output the results to the user below
+
+
+# End of Section B
