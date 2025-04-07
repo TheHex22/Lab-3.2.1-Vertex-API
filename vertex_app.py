ad634a7d3fc7161c422091966967703732bcf520
TheHex22 <hmoreno7@techexchange.in>
2025-04-07 12:41:43 -0600

    Update vertex_app.py

--- vertex_app.py
+++ vertex_app.py
@@ -1,6 +1,13 @@
+import os
 import streamlit as st
+import vertexai
+from vertexai.generative_models import GenerativeModel, GenerationConfig
 
+from dotenv import load_dotenv
 
+load_dotenv()
+
+vertexai.init(project=os.environ.get("hmoreno7techx25"), location="us-central1")
 
 st.title("Find your neighboring states")
 
@@ -9,6 +16,15 @@ users_state = st.text_input("Enter your state")
 
 # Section A: Add in your Vertex AI API call below
 
+model = GenerativeModel("gemini-1.5-flash-002" , system_instruction = f"Only include US states in your response. if {users_state} is not one of the 50 United States states do not respond.  (give answer in bullet points)")
+
+response = model.generate_content(
+    f"what are all neighboring states of {users_state}?",
+    generation_config={
+        "temperature": 0.2,
+        "max_output_tokens": 50,
+    }
+)
 
 # End of Section A
 
@@ -17,6 +33,6 @@ st.write("The neighboring states are:")
 
 
 # Section B:  Output the results to the user below
-
+st.write(response.text)
 
 # End of Section B
